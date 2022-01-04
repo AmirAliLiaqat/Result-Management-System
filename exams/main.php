9f8aa2d83af3a937cc5494d00e29af4e46a7cd6d
Amir Ali <amirliaqat2020@gmail.com>
2022-01-04 18:07:07 +0500

    Makes change in main.php file

--- exams/main.php
+++ exams/main.php
@@ -25,30 +25,42 @@
                     <img src="../images/english.png" class="card-img-top">
                     <div class="card-body text-center">
                         <h5 class="card-title">English</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">
+                            This papper has 5 multiple choice questions with 4 answers given below the question 
+                            every question has 10 marks and you have 1 minute to solve the papper after 1 minute
+                            the pappers submit automatically...
+                        </p>
                     </div>
                     <div class="card-footer text-center">
-                        <a class="btn btn-primary w-50" href="#" role="button">Start Now</a>
+                        <a class="btn btn-primary w-50" href="english.php" role="button">Start Now</a>
                     </div>
                 </div>
                 <div class="card border-success mx-2" style="border-radius: 15px;">
                     <img src="../images/urdu.png" class="card-img-top">
                     <div class="card-body text-center">
                         <h5 class="card-title">Urdu</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">
+                            This papper has 5 multiple choice questions with 4 answers given below the question 
+                            every question has 10 marks and you have 1 minute to solve the papper after 1 minute
+                            the pappers submit automatically...
+                        </p>
                     </div>
                     <div class="card-footer text-center">
-                        <a class="btn btn-primary w-50" href="#" role="button">Start Now</a>
+                        <a class="btn btn-primary w-50" href="urdu.php" role="button">Start Now</a>
                     </div>
                 </div>
                 <div class="card border-danger mx-2" style="border-radius: 15px;">
                     <img src="../images/maths.png" class="card-img-top">
                     <div class="card-body text-center">
                         <h5 class="card-title">Maths</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">
+                            This papper has 5 multiple choice questions with 4 answers given below the question 
+                            every question has 10 marks and you have 1 minute to solve the papper after 1 minute
+                            the pappers submit automatically...
+                        </p>
                     </div>
                     <div class="card-footer text-center">
-                        <a class="btn btn-primary w-50" href="#" role="button">Start Now</a>
+                        <a class="btn btn-primary w-50" href="maths.php" role="button">Start Now</a>
                     </div>
                 </div>
             </div><!--card-group-->
@@ -57,30 +69,42 @@
                     <img src="../images/phy.png" class="card-img-top">
                     <div class="card-body text-center">
                         <h5 class="card-title">Physics</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">
+                            This papper has 5 multiple choice questions with 4 answers given below the question 
+                            every question has 10 marks and you have 1 minute to solve the papper after 1 minute
+                            the pappers submit automatically...
+                        </p>
                     </div>
                     <div class="card-footer text-center">
-                        <a class="btn btn-primary w-50" href="#" role="button">Start Now</a>
+                        <a class="btn btn-primary w-50" href="physics.php" role="button">Start Now</a>
                     </div>
                 </div>
                 <div class="card border-success mx-2" style="border-radius: 15px;">
                     <img src="../images/chem.png" class="card-img-top">
                     <div class="card-body text-center">
                         <h5 class="card-title">Chemistry</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">
+                            This papper has 5 multiple choice questions with 4 answers given below the question 
+                            every question has 10 marks and you have 1 minute to solve the papper after 1 minute
+                            the pappers submit automatically...
+                        </p>
                     </div>
                     <div class="card-footer text-center">
-                        <a class="btn btn-primary w-50" href="#" role="button">Start Now</a>
+                        <a class="btn btn-primary w-50" href="chemistry.php" role="button">Start Now</a>
                     </div>
                 </div>
                 <div class="card border-danger mx-2" style="border-radius: 15px;">
                     <img src="../images/bio.png" class="card-img-top">
                     <div class="card-body text-center">
                         <h5 class="card-title">Biology</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">
+                            This papper has 5 multiple choice questions with 4 answers given below the question 
+                            every question has 10 marks and you have 1 minute to solve the papper after 1 minute
+                            the pappers submit automatically...
+                        </p>
                     </div>
                     <div class="card-footer text-center">
-                        <a class="btn btn-primary w-50" href="#" role="button">Start Now</a>
+                        <a class="btn btn-primary w-50" href="biology.php" role="button">Start Now</a>
                     </div>
                 </div>
             </div><!--card-group-->
@@ -89,30 +113,42 @@
                     <img src="../images/computer.png" class="card-img-top">
                     <div class="card-body text-center">
                         <h5 class="card-title">Computer</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">
+                            This papper has 5 multiple choice questions with 4 answers given below the question 
+                            every question has 10 marks and you have 1 minute to solve the papper after 1 minute
+                            the pappers submit automatically...
+                        </p>
                     </div>
                     <div class="card-footer text-center">
-                        <a class="btn btn-primary w-50" href="#" role="button">Start Now</a>
+                        <a class="btn btn-primary w-50" href="computer.php" role="button">Start Now</a>
                     </div>
                 </div>
                 <div class="card border-success mx-2" style="border-radius: 15px;">
                     <img src="../images/p.study.png" class="card-img-top">
                     <div class="card-body text-center">
                         <h5 class="card-title">Pak Study</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">
+                            This papper has 5 multiple choice questions with 4 answers given below the question 
+                            every question has 10 marks and you have 1 minute to solve the papper after 1 minute
+                            the pappers submit automatically...
+                        </p>
                     </div>
                     <div class="card-footer text-center">
-                        <a class="btn btn-primary w-50" href="#" role="button">Start Now</a>
+                        <a class="btn btn-primary w-50" href="pak-study.php" role="button">Start Now</a>
                     </div>
                 </div>
                 <div class="card border-danger mx-2" style="border-radius: 15px;">
                     <img src="../images/isl.png" class="card-img-top">
                     <div class="card-body text-center">
                         <h5 class="card-title">Islamiyat</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">
+                            This papper has 5 multiple choice questions with 4 answers given below the question 
+                            every question has 10 marks and you have 1 minute to solve the papper after 1 minute
+                            the pappers submit automatically...
+                        </p>
                     </div>
                     <div class="card-footer text-center">
-                        <a class="btn btn-primary w-50" href="#" role="button">Start Now</a>
+                        <a class="btn btn-primary w-50" href="islamiyat.php" role="button">Start Now</a>
                     </div>
                 </div>
             </div><!--card-group-->
