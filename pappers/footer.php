2b2dc3e7f29634d7387d432d178269f93c95a3ae
Amir Ali <amirliaqat2020@gmail.com>
2022-01-04 13:58:49 +0500

    Add the files in the pappers folder

---
+++ pappers/footer.php
@@ -0,0 +1,57 @@
+    <!-------------------- Footer Start -------------------->
+    <footer class="bg-dark">
+        <div class="container py-3">
+            <div class="row">
+                <div class="col-md-6">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0  m-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="http://localhost/result-management-system/index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Pages</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div><!--col-md-6-->
+                <div class="col-md-6 text-white">
+                    <i class="fab fa-facebook-f"></i>
+                    <i class="fab fa-twitter"></i>
+                    <i class="fab fa-youtube"></i>
+                    <i class="fab fa-instagram"></i>
+                    <i class="fab fa-google"></i>
+                    <i class="fab fa-github"></i>
+                    <!-- <img src="images/facebook.png">
+                    <img src="images/twitter.png">
+                    <img src="images/youtube.png">
+                    <img src="images/instagram.png">
+                    <img src="images/google.png">
+                    <img src="images/github.png"> -->
+                </div><!--col-md-6-->
+            </div><!--row-->
+            <div class="row mt-2">
+                <div class="col-md-12">
+                    <p class="text-white text-center">
+                        © Copyright All Rights Reserved 
+                        <a href="http://localhost/result-management-system"  class="text-white">www.result-management-system.com</a>
+                    </p>
+                </div><!--col-md-12-->
+            </div><!--row-->
+        </div><!--container-->
+    </footer>
+    <!-------------------- Footer End -------------------->
+
+    <!-------------------- Custom Js -------------------->
+    <!-- <script src="js/script.js"></script> -->
+
+    <!-------------------- Bootstrap Js -------------------->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
