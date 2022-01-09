df6cd39ed9f80cd069ea4d18e615891e4eac8e1e
Amir Ali <amirliaqat2020@gmail.com>
2022-01-09 13:09:01 +0500

    Add these files for put/read/delete and edit the data

---
+++ admin-panal/display-data.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard - Result Management System</title>
+    <link rel="shortcut icon" href="../images/logo.png">
+    <link rel="stylesheet" href="../css/style.css">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+</head>
+<body>
+
+    <?php require 'header.php'; ?>
+
+    <!-------------------- Section Start -------------------->
+    <section style="overflow:hidden">
+        <main>
+            <div class="container-fluid p-0">
+                <div class="row">
+                    <div class="col-md-2">
+                        <?php require 'sidebar.php'; ?>
+                    </div><!--col-md-2-->
+                    <div class="col-md-10">
+                        <h1 class="text-center text-primary mt-5" style="text-transform:uppercase; text-shadow: 2px 2px #000 !important; font-weight:bold">Students Result</h1>  
+                        <div class="row">
+                            <div class="col-md-12" style="overflow-x: auto">
+                                <table class="table table-bordered table-striped table-hover">
+                                    <thead>
+                                        <tr class="text-center bg-dark text-white">
+                                            <th>Stu Id</th>
+                                            <th>Class</th>
+                                            <th>Fitst Name</th>
+                                            <th>Last Name</th>
+                                            <th>Email</th>
+                                            <th>English</th>
+                                            <th>Urdu</th>
+                                            <th>Math</th>
+                                            <th>Pak Study</th>
+                                            <th>Islamiyat</th>
+                                            <th>Physics</th>
+                                            <th>Chemistry</th>
+                                            <th>Biology</th>
+                                            <th>Total</th>
+                                            <th>Otained</th>
+                                            <th>Percentage</th>
+                                            <th>Grade</th>
+                                            <th>Delete</th>
+                                            <th>Edit</th>
+                                        </tr>
+                                    </thead>
+                                    <?php  
+                                        require '../login/conn.php';
+                                        
+                                        $sql = "SELECT * FROM `student-data`";
+                                        $query = mysqli_query($conn, $sql) or die("Query Failed" . mysqli_connect_error());
+
+                                        if(mysqli_num_rows($query) > 0) {
+                                            while($row = mysqli_fetch_assoc($query)) {
+                                            
+                                    ?>
+                                    <tbody>
+                                        <tr class = "text-center">
+                                            <td> <?php echo $row['student_id']; ?> </td>
+                                            <td> <?php echo $row['class']; ?> </td>
+                                            <td> <?php echo $row['first_name']; ?> </td>
+                                            <td> <?php echo $row['last_name']; ?> </td>
+                                            <td> <?php echo $row['email']; ?> </td>
+                                            <td> <?php echo $row['eng']; ?> </td>
+                                            <td> <?php echo $row['urdu']; ?> </td>
+                                            <td> <?php echo $row['maths']; ?> </td>
+                                            <td> <?php echo $row['p.study']; ?> </td>
+                                            <td> <?php echo $row['isl']; ?> </td>
+                                            <td> <?php echo $row['phy']; ?> </td>
+                                            <td> <?php echo $row['chem']; ?> </td>
+                                            <td> <?php echo $row['bio']; ?> </td>
+                                            <td> <?php echo $row['total']; ?> </td>
+                                            <td> <?php echo $row['obtain']; ?> </td>
+                                            <td> <?php echo $row['percentage']; ?> </td>
+                                            <td> <?php echo $row['grade']; ?> </td>
+                                            <td> 
+                                                <button class="btn btn-danger w-100" style="width:10%;">
+                                                    <a href="delete-data.php?id=<?php echo $row['id']; ?>" class="text-white" style="text-decoration:none;">Delete</a>
+                                                </button> 
+                                            </td>
+                                            <td> 
+                                                <button class="btn btn-primary w-100" style="width:10%;">
+                                                    <a href="edit-data.php?id=<?php echo $row['id']; ?>" class="text-white" style="text-decoration:none;">Edit</a>
+                                                </button> 
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                    <?php
+                                        }
+                                    }
+                                    ?>
+                                </table>
+                                <button class="btn btn-info mb-2" type="submit" name="addNew" style="width:10%;">
+                                    <a href="Insert.php" class="text-white" style="text-decoration:none;">Add New</a>
+                                </button>
+                            </div>
+                        </div>
+                    </div><!--col-md-10-->
+                </div><!--row-->
+            </div><!--container-fluid-->
+        </main>
+    </section> 
+    <!-------------------- Section End -------------------->
+
+</body>
+</html>
---
+++ admin-panal/edit-data.php
@@ -0,0 +1,154 @@
+<?php 
+    require '../login/conn.php'; 
+
+    if(isset($_POST['update'])) {
+        $id = $_GET['id'];
+        $stuId = $_POST['stuId'];
+        $class = $_POST['class'];
+        $firstName = $_POST['fname'];
+        $lastName = $_POST['lname'];
+        $email = $_POST['email'];
+
+        $sql = "UPDATE `student-data` SET `student_id` = '$stuId', 
+        `class` = '$class', `first_name` = '$firstName', `last_name` = '$lastName', `email` = '$email' WHERE id = $id";
+
+        mysqli_query($conn, $sql) or die("Query Failed");
+
+        header('location: display-data.php');
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EDIT - Result Management System</title>
+    <link rel="shortcut icon" href="../images/logo.png">
+    <link rel="stylesheet" href="../css/style.css">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+</head>
+<body>
+
+    <?php require 'header.php'; ?>
+
+    <!-------------------- Section Start -------------------->
+    <section style="overflow:hidden">
+        <main>
+            <div class="container-fluid p-0">
+                <div class="row">
+                    <div class="col-md-2">
+                        <?php require 'sidebar.php'; ?>
+                    </div><!--col-md-2-->
+                    <div class="col-md-10">
+                        <h3 class="text-center text-uppercase fst-italic pt-5" style="color:blue">Edit the student data</h3>
+                        <?php
+
+                            $id = $_GET['id'];
+
+                            if(isset($_GET['id'])) {
+                                $sql = "SELECT * FROM `student-data` WHERE id = $id";
+                                $result = mysqli_query($conn, $sql) or die("Query Failed");
+
+                                if(mysqli_num_rows($result) > 0) {
+                                    while($row = mysqli_fetch_assoc($result)) {
+                        ?>
+                        <form action="" method="post">
+                            <div class="row m-2">
+                                <div class="col-md-6">
+                                    <label for="stuId" class="form-label">Student Id :</label>
+                                    <input type="text" name="stuId" id="" class="form-control" value="<?php echo $row['student_id']; ?>" required>
+                                </div><!--col-md-6-->
+                                <div class="col-md-6">
+                                    <label for="class" class="form-label">Class :</label>
+                                    <input type="text" name="class" id="" class="form-control" value="<?php echo $row['class']; ?>" class="form-control" required>
+                                </div><!--col-md-6-->
+                            </div><!--row-->
+                            <div class="row m-2">
+                                <div class="col-md-6">
+                                    <label for="fname" class="form-label">First Name :</label>
+                                    <input type="text" name="fname" id="" class="form-control" value="<?php echo $row['first_name']; ?>" required>
+                                </div><!--col-md-6-->
+                                <div class="col-md-6">
+                                    <label for="lname" class="form-label">Last Name :</label>
+                                    <input type="text" name="lname" id="" class="form-control" value="<?php echo $row['last_name']; ?>" required>
+                                </div><!--col-md-6-->
+                            </div><!--row-->
+                            <div class="row m-2">
+                                <div class="col-md-12">
+                                    <label class="form-label" for="email">Student Email :</label>
+                                    <input type="email" name="email" id="" class="form-control" value="<?php echo $row['email']; ?>" required>
+                                </div><!--col-md-12-->
+                            </div><!--row-->
+                            <div class="row m-2">
+                                <h6 class="my-3"><b>Type the Numbers :</b></h6>
+                                <div class="col-md-3">
+                                    <label for="eng" class="form-label">English :</label>
+                                    <input type="number" name="eng" id="" class="form-control" min="0" max="100" value="<?php echo $row['eng']; ?>" required>
+                                </div><!--col-md-3-->
+                                <div class="col-md-3">
+                                    <label for="urdu" class="form-label">Urdu :</label>
+                                    <input type="number" name="urdu" id="" class="form-control" min="0" max="100" value="<?php echo $row['urdu']; ?>" required>
+                                </div><!--col-md-3-->
+                                <div class="col-md-3">
+                                    <label for="maths" class="form-label">Maths :</label>
+                                    <input type="number" name="maths" id="" class="form-control" min="0" max="100" value="<?php echo $row['maths']; ?>" required>
+                                </div><!--col-md-3-->
+                                <div class="col-md-3">
+                                    <label for="pak-study" class="form-label">Pak Study :</label>
+                                    <input type="number" name="pak-study" id="" class="form-control" min="0" max="100" value="<?php echo $row['p.study']; ?>" required>
+                                </div><!--col-md-3-->
+                            </div><!--row-->
+                            <div class="row m-2">
+                                <div class="col-md-3">
+                                    <label for="isl" class="form-label">Islamiyat :</label>
+                                    <input type="number" name="isl" id="" class="form-control" min="0" max="100" value="<?php echo $row['isl']; ?>" required>
+                                </div><!--col-md-3-->
+                                <div class="col-md-3">
+                                    <label for="phy" class="form-label">Physics :</label>
+                                    <input type="number" name="phy" id="" class="form-control" min="0" max="100" value="<?php echo $row['phy']; ?>" required>
+                                </div><!--col-md-3-->
+                                <div class="col-md-3">
+                                    <label for="chem" class="form-label">Chemistry :</label>
+                                    <input type="number" name="chem" id="" class="form-control" min="0" max="100" value="<?php echo $row['chem']; ?>" required>
+                                </div><!--col-md-3-->
+                                <div class="col-md-3">
+                                    <label for="bio" class="form-label">Biology :</label>
+                                    <input type="number" name="bio" id="" class="form-control" min="0" max="100" value="<?php echo $row['bio']; ?>" required>
+                                </div><!--col-md-3-->
+                            </div><!--row-->
+                            <div class="row m-2">
+                                <div class="col-md-3">
+                                    <label for="total" class="form-label">Total :</label>
+                                    <input type="text" name="total" id="" class="form-control" value="<?php echo $row['total']; ?>">
+                                </div><!--col-md-3-->
+                                <div class="col-md-3">
+                                    <label for="obtain" class="form-label">Obtained :</label>
+                                    <input type="text" name="obtain" id="" class="form-control" value="<?php echo $row['obtain']; ?>">
+                                </div><!--col-md-3-->
+                                <div class="col-md-3">
+                                    <label for="per" class="form-label">Percentage :</label>
+                                    <input type="text" name="per" id="" class="form-control" value="<?php echo $row['percentage']; ?>">
+                                </div><!--col-md-3-->
+                                <div class="col-md-3">
+                                    <label for="grade" class="form-label">Grade :</label>
+                                    <input type="text" name="grade" id="" class="form-control" value="<?php echo $row['grade']; ?>">
+                                </div><!--col-md-3-->
+                            </div><!--row-->
+                            <button class="btn btn-primary mt-4 mx-4" type="submit" name="update" style="width:20%;">Update</button>
+                        </form>
+                        <?php
+                                    }
+                                }
+                            }
+                        ?>
+                    </div><!--col-md-10-->
+                </div><!--row-->
+            </div><!--container-fluid-->
+        </main>
+    </section> 
+    <!-------------------- Section End -------------------->
+
+</body>
+</html>
