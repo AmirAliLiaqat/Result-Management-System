c704064e6f32c852f28da1b8445f41bdb9514484
Amir Ali <amirliaqat2020@gmail.com>
2022-01-11 13:22:56 +0500

    Add jquery file to copy ul input fields

---
+++ js/jquery.js
@@ -0,0 +1,15 @@
+// $(document).ready(function(){
+//     $(body).on('click', function() {
+//         $("p").append(".papper_field");
+//     });
+//   });
+$(document).ready(function(){
+    // $(body).on('click', function() {
+        $(".add_more").on("click", function(){
+            $(".papper_field").append();
+        });
+    // });
+  });
+
+
+  
