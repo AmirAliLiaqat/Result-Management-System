0d993b2510fec2391da42dcd0d9401958dfff14a
Amir Ali <amirliaqat2020@gmail.com>
2022-01-11 13:19:44 +0500

    Add again these files in admin-panal

---
+++ admin-panal/main.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exams - Result Management System</title>
+    <link rel="shortcut icon" href="../images/logo.png">
+    <link rel="stylesheet" href="../css/style.css">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+</head>
+<body>
+
+    <?php include 'header.php'; ?>
+
+   
+    <!-------------------- Section Start -------------------->
+    <section>
+        <div class="container my-4">
+            <h1 class="text-center my-5">__________Select The Papper You Want To Do__________</h1>
+            <div class="card-group my-4">
+                <div class="card border-primary mx-2" style="border-radius: 15px;">
+                    <img src="../images/english.png" class="card-img-top">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">English</h5>
+                        <p class="card-text">
+                            This papper has 5 multiple choice questions with 4 answers given below the question 
+                            every question has 10 marks and you have 1 minute to solve the papper after 1 minute
+                            the pappers submit automatically...
+                        </p>
+                    </div>
+                    <div class="card-footer text-center">
+                        <a class="btn btn-primary w-50" href="english.php" role="button">Start Now</a>
+                    </div>
+                </div>
+                <div class="card border-success mx-2" style="border-radius: 15px;">
+                    <img src="../images/urdu.png" class="card-img-top">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Urdu</h5>
+                        <p class="card-text">
+                            This papper has 5 multiple choice questions with 4 answers given below the question 
+                            every question has 10 marks and you have 1 minute to solve the papper after 1 minute
+                            the pappers submit automatically...
+                        </p>
+                    </div>
+                    <div class="card-footer text-center">
+                        <a class="btn btn-primary w-50" href="urdu.php" role="button">Start Now</a>
+                    </div>
+                </div>
+                <div class="card border-danger mx-2" style="border-radius: 15px;">
+                    <img src="../images/maths.png" class="card-img-top">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Maths</h5>
+                        <p class="card-text">
+                            This papper has 5 multiple choice questions with 4 answers given below the question 
+                            every question has 10 marks and you have 1 minute to solve the papper after 1 minute
+                            the pappers submit automatically...
+                        </p>
+                    </div>
+                    <div class="card-footer text-center">
+                        <a class="btn btn-primary w-50" href="maths.php" role="button">Start Now</a>
+                    </div>
+                </div>
+            </div><!--card-group-->
+            <div class="card-group my-4">
+                <div class="card border-primary mx-2" style="border-radius: 15px;">
+                    <img src="../images/phy.png" class="card-img-top">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Physics</h5>
+                        <p class="card-text">
+                            This papper has 5 multiple choice questions with 4 answers given below the question 
+                            every question has 10 marks and you have 1 minute to solve the papper after 1 minute
+                            the pappers submit automatically...
+                        </p>
+                    </div>
+                    <div class="card-footer text-center">
+                        <a class="btn btn-primary w-50" href="physics.php" role="button">Start Now</a>
+                    </div>
+                </div>
+                <div class="card border-success mx-2" style="border-radius: 15px;">
+                    <img src="../images/chem.png" class="card-img-top">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Chemistry</h5>
+                        <p class="card-text">
+                            This papper has 5 multiple choice questions with 4 answers given below the question 
+                            every question has 10 marks and you have 1 minute to solve the papper after 1 minute
+                            the pappers submit automatically...
+                        </p>
+                    </div>
+                    <div class="card-footer text-center">
+                        <a class="btn btn-primary w-50" href="chemistry.php" role="button">Start Now</a>
+                    </div>
+                </div>
+                <div class="card border-danger mx-2" style="border-radius: 15px;">
+                    <img src="../images/bio.png" class="card-img-top">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Biology</h5>
+                        <p class="card-text">
+                            This papper has 5 multiple choice questions with 4 answers given below the question 
+                            every question has 10 marks and you have 1 minute to solve the papper after 1 minute
+                            the pappers submit automatically...
+                        </p>
+                    </div>
+                    <div class="card-footer text-center">
+                        <a class="btn btn-primary w-50" href="biology.php" role="button">Start Now</a>
+                    </div>
+                </div>
+            </div><!--card-group-->
+            <div class="card-group my-4">
+                <div class="card border-primary mx-2" style="border-radius: 15px;">
+                    <img src="../images/computer.png" class="card-img-top">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Computer</h5>
+                        <p class="card-text">
+                            This papper has 5 multiple choice questions with 4 answers given below the question 
+                            every question has 10 marks and you have 1 minute to solve the papper after 1 minute
+                            the pappers submit automatically...
+                        </p>
+                    </div>
+                    <div class="card-footer text-center">
+                        <a class="btn btn-primary w-50" href="computer.php" role="button">Start Now</a>
+                    </div>
+                </div>
+                <div class="card border-success mx-2" style="border-radius: 15px;">
+                    <img src="../images/p.study.png" class="card-img-top">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Pak Study</h5>
+                        <p class="card-text">
+                            This papper has 5 multiple choice questions with 4 answers given below the question 
+                            every question has 10 marks and you have 1 minute to solve the papper after 1 minute
+                            the pappers submit automatically...
+                        </p>
+                    </div>
+                    <div class="card-footer text-center">
+                        <a class="btn btn-primary w-50" href="pak-study.php" role="button">Start Now</a>
+                    </div>
+                </div>
+                <div class="card border-danger mx-2" style="border-radius: 15px;">
+                    <img src="../images/isl.png" class="card-img-top">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Islamiyat</h5>
+                        <p class="card-text">
+                            This papper has 5 multiple choice questions with 4 answers given below the question 
+                            every question has 10 marks and you have 1 minute to solve the papper after 1 minute
+                            the pappers submit automatically...
+                        </p>
+                    </div>
+                    <div class="card-footer text-center">
+                        <a class="btn btn-primary w-50" href="islamiyat.php" role="button">Start Now</a>
+                    </div>
+                </div>
+            </div><!--card-group-->
+            <div class="moreCourses">
+                <a class="btn btn-primary" href="#" role="button">More Courses</a>
+            </div><!--moreCourses-->
+        </div><!--container-->
+    </section>
+    <!-------------------- Section End -------------------->
+
+    <?php include 'footer.php'; ?>
+
+</body>
+</html>
